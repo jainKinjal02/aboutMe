d8be03781ea7645a0a7cb9c4735c59a779ec6942
Kinjal Jain <jainkinjal021196@gmail.com>
2024-05-27 21:46:43 +0530

    Changes title

--- app/head.js
+++ app/head.js
@@ -1,6 +1,6 @@
 const Head = () => (
   <>
-    <title>Metaversus By Kinjal</title>
+    <title>When I am not working</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
   </>
